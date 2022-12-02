09b90f142f0d8757a0b4b7c8f16a976d2e60d62e
mdwivedim <mdwivedim@outlook.com>
2022-12-02 11:30:56 +0000

    converted to maven project

---
+++ src/day4/Tester.java
@@ -0,0 +1,26 @@
+package day4;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Tester {
+	public static void main(String[] args) { 
+		Logger logger = LogFileHandler.setLogger(null,Logger.getLogger(Tester.class.getName()));
+		logger.log(Level.WARNING, "Warning Text!");
+		logger.log(Level.CONFIG, "Config Text!");
+		logger.log(Level.ALL, "All Text!");
+		logger.log(Level.FINE, "A fine Text!");
+		logger.log(Level.FINER, "A finer Text!");
+		logger.log(Level.FINEST, "A finest Text!");
+		logger.log(Level.INFO, "An info Text!");
+		logger.log(Level.SEVERE, "Severe Text!");
+		logger.log(Level.OFF, "No Loggings!");
+		
+		try {
+			UserAuthentication authentication = new UserAuthentication();
+			authentication.loginUser("Jon", "12345");
+			System.out.println("User has logged in successfully");
+		} catch(Exception exception) {
+			System.out.println(exception.getMessage());
+		}
+	}
+}
---
+++ src/day4/UserAuthentication.java
@@ -0,0 +1,20 @@
+package day4;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class UserAuthentication {
+	public Logger logger = LogFileHandler.setLogger(null,Logger.getLogger(Tester.class.getName()));
+
+	public void loginUser(String username, String password) throws Exception {
+		try {
+			if (username.isBlank() || password.isBlank()) {
+				throw new Exception("Service.INVALID_CREDENTIALS");
+			}
+			logger.info(username + " logged in successfully");
+		} catch (Exception exception) {
+			logger.log(Level.SEVERE, exception.getMessage() + " " + exception.getCause());
+			throw exception;
+		}
+	}
+}
