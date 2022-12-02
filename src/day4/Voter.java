918b2433fd032bdf242ea514ca811b5004793a78
mdwivedim <mdwivedim@outlook.com>
2022-12-02 12:21:57 +0000

    VoterTest

---
+++ src/day4/Voter.java
@@ -0,0 +1,10 @@
+package day4;
+
+public class Voter {
+	public boolean isVoterValid(int age) throws Exception {
+		if (age < 0) {
+			throw new Exception("Invalid age");
+		}
+		return age >= 18;
+	}
+}
---
+++ src/day4/VoterTest.java
@@ -0,0 +1,39 @@
+package day4;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class VoterTest {
+	@Test
+	public void validateVoterAgeValidTest() throws Exception {
+		int age = 19;
+		Voter voter = new Voter();
+		boolean result = voter.isVoterValid(age);
+		Assertions.assertTrue(result);
+	}
+
+	@Test
+	public void validateVoterAgeInvalidTest() throws Exception {
+		int age = 10;
+		Voter voterValidator = new Voter();
+		boolean result = voterValidator.isVoterValid(age);
+		Assertions.assertFalse(result);
+	}
+
+	@Test
+	public void validateVoterAgeInvalidTest2() throws Exception {
+		int age = -14;
+		Voter voter = new Voter();
+		Exception exception = Assertions.assertThrows(Exception.class, () -> voter.isVoterValid(age));
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = { "19, true", "20,true", "17,false", "16,false" })
+	public void validateVoterAgeTestParameter(int age, boolean expected) throws Exception {
+		Voter voter = new Voter();
+		boolean actual = voter.isVoterValid(age);
+		Assertions.assertEquals(expected, actual);
+	}
+}
