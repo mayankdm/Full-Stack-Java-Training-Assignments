36bf631f96e18af2eaa000e634bc5aaf5d9a4046
mdwivedim <mdwivedim@outlook.com>
2022-12-22 10:58:09 +0000

    bug

--- src/main/resources/public/todo-project/src/css-examples9/index.js
+++ src/main/resources/public/todo-project/src/css-examples9/index.js
@@ -26,49 +26,29 @@ for (var i = 0; i < number.length; i++) {
 }
 for (var i = 0; i < operator.length; i++) {
     operator[i].addEventListener("click", function (e) {
-
-        // storing current input string and its last character in variables - used later
         var currentString = input.innerHTML;
         var lastChar = currentString[currentString.length - 1];
-
-        // if last character entered is an operator, replace it with the currently pressed one
         if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
             var newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
             input.innerHTML = newString;
         } else if (currentString.length == 0) {
-            // if first key pressed is an opearator, don't do anything
             console.log("enter a number first");
         } else {
-            // else just add the operator pressed to the input
             input.innerHTML += e.target.innerHTML;
         }
 
     });
 }
-
-// on click of 'equal' button
 if (result) {
     result.addEventListener("click", function () {
-
-        // this is the string that we will be processing eg. -10+26+33-56*34/23
         var inputString = input.innerHTML;
-
-        // forming an array of numbers. eg for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
         var numbers = inputString.split(/\+|\-|\×|\÷/g);
-
-        // forming an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
-        // first we replace all the numbers and dot with empty string and then split
         var operators = inputString.replace(/[0-9]|\./g, "").split("");
 
         console.log(inputString);
         console.log(operators);
         console.log(numbers);
         console.log("----------------------------");
-
-        // now we are looping through the array and doing one operation at a time.
-        // first divide, then multiply, then subtraction and then addition
-        // as we move we are alterning the original numbers and operators array
-        // the final element remaining in the array will be the output
 
         var divide = operators.indexOf("÷");
         while (divide != -1) {
@@ -93,20 +73,16 @@ if (result) {
 
         var add = operators.indexOf("+");
         while (add != -1) {
-            // using parseFloat is necessary, otherwise it will result in string concatenation :)
             numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
             operators.splice(add, 1);
             add = operators.indexOf("+");
         }
 
-        input.innerHTML = numbers[0]; // displaying the output
+        input.innerHTML = numbers[0];
 
-        resultDisplayed = true; // turning flag if result is displayed
+        resultDisplayed = true;
     });
 }
-
-
-// clearing the input on press of clear
 if (clear) {
     clear.addEventListener("click", function () {
         input.innerHTML = "";
