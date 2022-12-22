876e7b5c049786a7dc996884c8abdf7cf824ddbe
mdwivedim <mdwivedim@outlook.com>
2022-12-22 12:19:27 +0000

    calculator

--- src/main/resources/public/todo-project/src/css-examples9/index.js
+++ src/main/resources/public/todo-project/src/css-examples9/index.js
@@ -1,45 +1,45 @@
 "use strict";
+window.onload = function () {
 
-var input = document.getElementById('input'),
-    number = document.querySelectorAll('.numbers div'),
-    operator = document.querySelectorAll('.operators div'),
-    result = document.getElementById('result'),
-    clear = document.getElementById('clear'),
-    resultDisplayed = false;
+    var input = document.getElementById('input'),
+        number = document.querySelectorAll('.numbers div'),
+        operator = document.querySelectorAll('.operators div'),
+        result = document.getElementById('result'),
+        clear = document.getElementById('clear'),
+        resultDisplayed = false;
+    for (var i = 0; i < number.length; i++) {
+        number[i].addEventListener("click", function (e) {
+            var currentString = input.innerHTML;
+            var lastChar = currentString[currentString.length - 1];
+            if (resultDisplayed === false) {
+                input.innerHTML += e.target.innerHTML;
+            } else if (resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
+                resultDisplayed = false;
+                input.innerHTML += e.target.innerHTML;
+            } else {
+                resultDisplayed = false;
+                input.innerHTML = "";
+                input.innerHTML += e.target.innerHTML;
+            }
 
-for (var i = 0; i < number.length; i++) {
-    number[i].addEventListener("click", function (e) {
-        var currentString = input.innerHTML;
-        var lastChar = currentString[currentString.length - 1];
-        if (resultDisplayed === false) {
-            input.innerHTML += e.target.innerHTML;
-        } else if (resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
-            resultDisplayed = false;
-            input.innerHTML += e.target.innerHTML;
-        } else {
-            resultDisplayed = false;
-            input.innerHTML = "";
-            input.innerHTML += e.target.innerHTML;
-        }
+        });
+    }
+    for (var i = 0; i < operator.length; i++) {
+        operator[i].addEventListener("click", function (e) {
+            var currentString = input.innerHTML;
+            var lastChar = currentString[currentString.length - 1];
+            if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
+                var newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
+                input.innerHTML = newString;
+            } else if (currentString.length == 0) {
 
-    });
-}
-for (var i = 0; i < operator.length; i++) {
-    operator[i].addEventListener("click", function (e) {
-        var currentString = input.innerHTML;
-        var lastChar = currentString[currentString.length - 1];
-        if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
-            var newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
-            input.innerHTML = newString;
-        } else if (currentString.length == 0) {
-            console.log("enter a number first");
-        } else {
-            input.innerHTML += e.target.innerHTML;
-        }
+                console.log("enter a number first");
+            } else {
+                input.innerHTML += e.target.innerHTML;
+            }
 
-    });
-}
-if (result) {
+        });
+    }
     result.addEventListener("click", function () {
         var inputString = input.innerHTML;
         var numbers = inputString.split(/\+|\-|\×|\÷/g);
@@ -82,9 +82,9 @@ if (result) {
 
         resultDisplayed = true;
     });
-}
-if (clear) {
+
+
     clear.addEventListener("click", function () {
         input.innerHTML = "";
     })
-}
+};
